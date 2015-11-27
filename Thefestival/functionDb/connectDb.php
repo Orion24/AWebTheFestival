3ad71fda0bebbd3980e2a130a5d63e3d6a7f668e
Orion24 <nicolas.bertrand74@gmail.com>
2015-11-27 08:24:58 +0100

    Ajout de l'entête
    Nom, prénom, date, version

--- Thefestival/functionDb/connectDb.php
+++ Thefestival/functionDb/connectDb.php
@@ -1,23 +1,28 @@
 <?php
-DEFINE('DB_HOST', "127.0.0.1");
-DEFINE('DB_NAME', "festival");
-DEFINE('DB_USER', "userFestival");
-DEFINE('DB_PASS', "SuperUser");
-function getDb()
-{
-    static $dbb = null;
-
-    if($dbb === null)
+    /*
+     * Auteur : Bertrand Nicolas
+     * Date : 27.11.2011-2015
+     * Version : 0.8
+     */
+    DEFINE('DB_HOST', "127.0.0.1");
+    DEFINE('DB_NAME', "festival");
+    DEFINE('DB_USER', "userFestival");
+    DEFINE('DB_PASS', "SuperUser");
+    function getDb()
     {
-        try
+        static $dbb = null;
+
+        if($dbb === null)
         {
-            $dbb = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.'', DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-            $dbb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            try
+            {
+                $dbb = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.'', DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+                $dbb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
         }
-        catch (PDOException $e)
-        {
-            die('Erreur : ' . $e->getMessage());
-        }
+        return $dbb;
     }
-    return $dbb;
-}
